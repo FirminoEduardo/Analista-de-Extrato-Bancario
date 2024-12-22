e963b977b27b2a78d02a8641a1cc72eeb91e3727
FirminoEduardo <dudu.lf@outlook.com>
2024-12-22 14:07:05 -0300

    Uma unidade de teste falha para o parser CSV

---
+++ src/test/java/BankStatementCSVParserTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BankStatementCSVParserTest {
+    private final BankStatementCSVParser parser = new BankStatementCSVParser();
+
+    @Test
+    public void shouldParseOneCorrectLine() throws Exception {
+        Assert.fail("Not yet implemented");
+    }
+}
